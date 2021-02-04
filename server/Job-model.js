97ecdcc472e066dba08e2970a6557fad031d5caa
Victor Wei <vw1262@gmail.com>
2021-02-04 15:57:05 -0600

    update

--- server/Job-model.js
+++ server/Job-model.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const JobSchema = new Schema({
-    title: String,
-    description: String,
-    price: String,
-    skills: Array,
-    otherSkills: String,
-    paymentForms: Array
+  title: String,
+  description: String,
+  price: String,
+  skills: Array,
+  otherSkills: String,
+  paymentForms: Array
 });
 
 module.exports = mongoose.model('Job', JobSchema);
