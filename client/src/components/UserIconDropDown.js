bae9379f5a6233c2fdc8399180bd1cc9cdba2aeb
Abduboriy <aaauzbek@gmail.com>
2021-03-26 09:45:59 -0500

    changes revert

--- client/src/components/UserIconDropDown.js
+++ client/src/components/UserIconDropDown.js
@@ -11,12 +11,7 @@ export default function UserIconDropDown() {
   return (
     isAuthenticated && (
       <NavDropdown title={<Image src={user.picture} style={{'height': '50px'}} className="mr-3" roundedCircle/>}>
-        <NavDropdown.Item>
-          <Link to="/dashboard" className="dropdown-item text-center">Profile</Link>
-        </NavDropdown.Item>
-        <NavDropdown.Item>
-          <Link to="/createfreelancer" className="dropdown-item text-center">Create Freelancer<br></br>Account</Link>
-        </NavDropdown.Item>
+        <Link to="/dashboard" className="dropdown-item text-center">Profile</Link>
         <NavDropdown.Item>
           <LogoutButton />
         </NavDropdown.Item>
